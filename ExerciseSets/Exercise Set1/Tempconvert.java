b69682669a228a4e38707fcc339858630434a0b3
NoelSaludo <23-08992@g.batstate-u.edu.ph>
2024-09-30 08:50:28 +0800

    readded exercise sets

---
+++ ExerciseSets/Exercise Set1/Tempconvert.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+
+/**
+ * set3
+ */
+public class Tempconvert {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a celsuis degrees: ");
+        float celsuis = scanner.nextFloat();
+        float farenheit = (celsuis * 9)/5 + 32;
+        String output = String.format("%.2f Celsius is %.2f in Farenheit", celsuis, farenheit);
+        System.out.println(output);
+        scanner.close();
+    } 
+}
