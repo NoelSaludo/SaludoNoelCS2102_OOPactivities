d0fad950a0d7953b46c24a268b0f8a98af27b29e
NoelSaludo <23-08992@g.batstate-u.edu.ph>
2024-10-07 10:02:37 +0800

    renamed inova to innova

--- ExerciseSets/Exercise Set2/Main.java
+++ ExerciseSets/Exercise Set2/Main.java
@@ -5,21 +5,21 @@ class Main {
 
     public static void main(String[] args) {
         /* Constructor */
-        Car inova = new Car("Red", 190000.00, 'L');
+        Car innova = new Car("Red", 190000.001, 'L');
 
         /* toString method */
-        System.out.println(inova);
+        System.out.println(innova);
 
         /* Setters */
-        inova.setColor("Green");
-        inova.setPrice(200000.00);
-        inova.setSize('M');
-        System.out.println(inova);
+        innova.setColor("Green");
+        innova.setPrice(200000.00);
+        innova.setSize('M');
+        System.out.println(innova);
 
         /* Getters */
-        String color = inova.getColor();
-        double price = inova.getPrice();
-        char size = inova.getSize();
+        String color = innova.getColor();
+        double price = innova.getPrice();
+        char size = innova.getSize();
         System.out.println("Color: " + color);
         System.out.println("Price: " + price);
         System.out.println("Size: " + size);
